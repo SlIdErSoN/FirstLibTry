6f3adb985a48e4f46d313884ee065403de9dec95
SlIdErSoN <sliderson01@mail.ru>
2022-01-14 11:47:57 +0700

    MyFirstRepository

---
+++ MyFirstRepository/FirstLibrary/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Chapter2.Task3;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Chapter2.Task3.Program.run();
+        }
+    }
+}
